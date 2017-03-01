d7436c394139d7d10604ae1092610f8d949447f5
SofíaAlmeida <sofiil97@gmail.com>
2017-03-01 13:18:53 +0100

    Añadida documentación

--- java/Napakalaki/src/napakalaki/Prize.java
+++ java/Napakalaki/src/napakalaki/Prize.java
@@ -1,31 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package napakalaki;
 
 /**
  *
- * @author usuario
+ * @author Sofía Almeida Bruno
+ * @author María Victoria Granados Pozo
  */
 public class Prize {
     private int treasures;
     private int level;
     
+    /**
+     * 
+     * Constructor
+     * @param treasures número de tesoros ganados
+     * @param level número de niveles ganados
+     */
     public Prize(int treasures, int level) {
         this.treasures = treasures;
         this.level = level;
     }
     
+    /**
+     * 
+     * Consultor de tesoros
+     * @return número de tesoros ganados
+     */
     public int getTreasures() {
         return treasures;
     }
     
+    /**
+     * 
+     * Consultor de niveles
+     * @return número de niveles ganados
+     */
     public int getLevel() {
         return level;
     }
     
+    /**
+     * 
+     * @return String con la información del objeto
+     */
     public String toString() {
         return "Treasures = " + Integer.toString(treasures) +
                 "\nLevels = " + Integer.toString(level);
