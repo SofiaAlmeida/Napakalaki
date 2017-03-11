bfe8c761e549f2786f814217e159f687d78aacee
mvictoria1997 <mvictoria1997@correo.ugr.es>
2017-03-11 23:34:57 +0100

    R: Arreglar inicializadores y to_s

--- ruby/Napakalaki/lib/bad_consequence.rb
+++ ruby/Napakalaki/lib/bad_consequence.rb
@@ -1,4 +1,5 @@
 #encoding: utf-8
+require_relative "treasure_kind.rb"
 
 class BadConsequence
   attr_reader :text
@@ -8,6 +9,9 @@ class BadConsequence
   attr_reader :death
   attr_reader :specificHiddenTreasures
   attr_reader :specificVisibleTreasures
+  
+   @specificHiddenTreasures = Array.new
+   @specificVisibleTreasures = Array.new
   
   def initialize(aText, someLevels, someVisibleTreasures, someHiddenTreasures, 
       someSpecificVisibleTreasures, someSpecificHiddenTreasures, death)
@@ -20,34 +24,16 @@ class BadConsequence
     @death = death
   end
  
-  def BadConsequence.newLevelNumberOfTreasures(aText, someLevels, someVisibleTreasures, someHiddenTreasures)
-    @text = aText
-    @levels = someLevels
-    @nVisibleTreasures = someVisibleTreasures
-    @nHiddenTreasures = someHiddenTreasures
-    @death = false
-    @specificHiddenTreasures = Array.new
-    @specificVisibleTreasures = Array.new
+  def self.newLevelNumberOfTreasures(aText, someLevels, someVisibleTreasures, someHiddenTreasures)
+    new(aText, someLevels, someVisibleTreasures, someHiddenTreasures, nil,nil,false)
   end
 
-  def BadConsequence.newLevelSpecificTreasures(aText, someLevels, someSpecificVisibleTreasures, someSpecificHiddenTreasures)
-    @text = aText
-    @levels = someLevels
-    @nVisibleTreasures = 0
-    @nHiddenTreasures = 0
-    @death = false
-    @specificHiddenTreasures = someSpecificVisibleTreasures
-    @specificVisibleTreasures = someSpecificHiddenTreasures
+  def self.newLevelSpecificTreasures(aText, someLevels, someSpecificVisibleTreasures, someSpecificHiddenTreasures)
+    new(aText, someLevels, 0, 0, someSpecificVisibleTreasures, someSpecificHiddenTreasures, false)
   end
   
-  def BadConsequence.newDeath(aText)
-    @text = aText
-    @levels = 0
-    @nVisibleTreasures = 0
-    @nHiddenTreasures = 0
-    @death = true
-    @specificHiddenTreasures = Array.new
-    @specificVisibleTreasures = Array.new
+  def self.newDeath(aText)
+    new(aText, 0, 0, 0, nil, nil, true)
   end
 
   
@@ -56,9 +42,21 @@ class BadConsequence
     # No sé cómo haremos los constructores...
     #Dejo esto por aquí para que nos sirva de base luego, pero supongo que habrá 
     #que poner algún if-else igual que en java
-    "Texto: #{@text} \nNiveles: #{@levels} \n Tesoros visibles: #{@nVisibleTreasures} \n
-      Tesoros ocultos: #{@nHiddenTreasures} \n Muerte: #{@death} \n Tesoros ocultos específicos:
-      #{@specificHiddenTreasures} \n Tesoros visibles específicos: #{@specificVisibleTreasures}"
+   # "Texto: #{@text} \nNiveles: #{@levels} \n Tesoros visibles: #{@nVisibleTreasures} \n
+   #  Tesoros ocultos: #{@nHiddenTreasures} \n Muerte: #{@death} \n Tesoros ocultos específicos:
+   # #{@specificHiddenTreasures} \n Tesoros visibles específicos: #{@specificVisibleTreasures}"
+    
+    if @death==false
+      if @specificVisibleTreasures.nil? and @specificHiddenTreasures.nil? #La linea de abajo hay que ponerla así sino hace un salto de linea mas
+        "\n\tTexto: #{@text} \n\tNiveles: #{@levels} \n\tTesoros visibles: #{@nVisibleTreasures}\n\tTesoros ocultos: #{@nHiddenTreasures}"
+      else
+        "\n\tTexto: #{@text} \n\tNiveles: #{@levels} \n\tTesoros ocultos específicos:
+        #{@specificHiddenTreasures} \n\tTesoros visibles específicos: #{@specificVisibleTreasures}"
+      end
+    else
+      "\n\tTexto: #{@text} \n\tMuerto"
+    end
+    
   end
   
   private_class_method :new # Comprobar posición
