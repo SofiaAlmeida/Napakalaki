c42449504ef0f7cddbaf36ebed1716a453ed26a4
mvictoria1997 <mvictoria1997@correo.ugr.es>
2017-03-15 22:07:38 +0100

    J:Creación de la clase Trasures

---
+++ java/Napakalaki/src/napakalaki/Treasure.java
@@ -0,0 +1,47 @@
+package napakalaki;
+
+/**
+ * 
+ * @author Sofía Almeida Bruno
+ * @author María Victoria Granados Pozo
+ */
+public class Treasure {
+    private String name;
+    private int bonus;
+    private TreasureKind type;
+    
+    /**
+     * 
+     * Constructor
+     * @param n nombre de la carta de tesoros
+     * @param bonus puntuación adicional del tesoro
+     * @param t tipo de tesoro
+     */
+    public Treasure(String n, int bonus, TreasureKind t){
+        name = n;
+        this.bonus = bonus;
+        type = t;
+    }
+    
+    /**
+     * 
+     * Consultor del nombre
+     * @return nombre de la carta de tesoros
+     */
+    public String getName(){
+        return name;
+    }
+    
+    /**
+     * 
+     * Consultor del bonus 
+     * @return bonus del tesoro
+     */
+    public int getBonus(){
+        return bonus;
+    }
+    
+    public TreasureKind getType(){
+        return type;
+    }
+}
