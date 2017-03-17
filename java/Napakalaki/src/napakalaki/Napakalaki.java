fd103fbf14048b03ff152d444314f9161bb06bdf
mvictoria1997 <mvictoria1997@correo.ugr.es>
2017-03-17 18:27:19 +0100

    J:Crear clase Napakalaki y declaración e implementación de las funciones indicadas

---
+++ java/Napakalaki/src/napakalaki/Napakalaki.java
@@ -0,0 +1,77 @@
+package napakalaki;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sofía Almeida Bruno
+ * @author María Victoria Granados Pozo
+ */
+public class Napakalaki {
+    private static final Napakalaki instance = new Napakalaki();
+    private CardDealer dealer;
+    private ArrayList<Player> players;
+    private Player currentPlayer;
+    private Monster currentMonster;
+    
+    private Napakalaki(){
+       /* currentPlayer = null;
+        currentMonster = null;
+        players = new ArrayList();*/
+    }
+    
+    private void initPlayers(ArrayList<String> names){
+        
+    }
+    
+    /*private Player nextPlayer(){
+        
+    }
+    
+    private boolean nextTurnAllowed(){
+        
+    }
+    
+    private void setEnemies(){
+        
+    }
+    
+    public Napakalaki getInstance(){
+        
+    }
+    
+    public CombatResult developCombat(){
+        
+    }
+    
+    public void discardVisibleTreasures(ArrayList<Treasure> treasures){
+        
+    }
+    
+    public void discardHiddenTreasures(ArrayList<Treasure> treasures){
+        
+    }
+    
+    public void makeTreasuresVisible(ArrayList<Treasure> treasures){
+        
+    }
+    
+    public void initGame(ArrayList<String> players){
+        
+    }*/
+    
+    /*public Player getCurrentPlayer(){
+        
+    }
+    
+    public Monster getCurrentMonster(){
+        
+    }
+    
+    public boolean nextTurn(){
+        
+    }
+    
+    public boolean endOfGame (CombatResult result){
+        
+    }*/
+}
