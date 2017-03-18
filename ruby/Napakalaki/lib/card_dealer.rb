9c7f04db5e4ca199d78ddd3f5aa378265261146c
mvictoria1997 <mvictoria1997@correo.ugr.es>
2017-03-18 16:39:15 +0100

    R:Creación de la clase con algunas funciones

---
+++ ruby/Napakalaki/lib/card_dealer.rb
@@ -0,0 +1,40 @@
+#encoding: utf-8
+#
+#Sofía Almeida Bruno
+#María Victoria Granados Pozo
+
+include Singleton
+
+class CardDealer
+  
+  def initialize
+    @unsuedMonsters = Array.new
+    @usedMonster = Array.new
+    @usedTreasures = Array.new
+    @unsusedTreasures = Array.new
+    @instance = nil
+  end
+  
+  def getInstance
+    
+    
+  end
+  
+  private
+  def initTreasureCardDeck
+    
+  end
+  
+  def initMonsterCardDeck
+    
+  end
+  
+  def shuffleTreasures
+    
+  end
+  
+  def suffleMonster
+    
+  end
+
+end
