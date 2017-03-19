adcc731a52249a8acb29edf76d515e51628b2a4a
mvictoria1997 <mvictoria1997@correo.ugr.es>
2017-03-19 17:44:45 +0100

    R:Creación y declaración de las funciones de la clase Napakalaki

---
+++ ruby/Napakalaki/lib/napakalaki.rb
@@ -0,0 +1,57 @@
+#encoding: utf-8
+#
+#Sofía Almeida Bruno
+#María Victoria Granados Pozo
+
+#No se si se pone así lo de clase singleton
+include Singleton
+class Napakalaki
+  def initialize 
+    @currentMonster
+    @dealer
+    @instance
+    @players
+    @currentPlayer
+  end
+  def getInstance
+    
+  end
+  
+  def developCombat
+    
+  end
+  
+  def discardVisibleTreasures(treasures)
+    
+  end
+  
+  def discardHiddenTreasures(treasures)
+    
+  end
+  
+  def makeTreasuresVisible(treasures)
+    
+  end
+  
+  def initGame(players)
+    
+  end
+  
+  def getCurrentPlayer
+    
+  end
+  
+  def getCurrentMonster
+    
+  end
+  
+  def nextTurn
+    
+  end
+  
+  def endOfGame(result)
+    
+  end
+  
+  
+end
